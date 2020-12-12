f91143ec0808635902df6985291093f14a566e32
yagarwal1 <y.agarwal1@nuigalway.ie>
2020-12-12 23:15:46 +0000

    Testing commit

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -10,7 +10,7 @@ import re
 ### result. Name them according to the task ID as in the three
 ### examples below. Delete the three examples. The tasks you choose
 ### must be in the data/training directory, not data/evaluation.
-def solve_6a1e5592(x):
+def solve_sdffgdf(x):
     return x
 
 def solve_b2862040(x):
